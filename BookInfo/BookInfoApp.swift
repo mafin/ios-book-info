587ec3f3bcc498fbadce2367ae3ccb90104b0640
Petr Jenin <petr.enin@gmail.com>
2024-11-09 12:33:20 +0100

    Initial Commit

---
+++ BookInfo/BookInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookInfoApp.swift
+//  BookInfo
+//
+//  Created by Petr on 09.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookInfoTests/BookInfoTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookInfoTests.swift
+//  BookInfoTests
+//
+//  Created by Petr on 09.11.2024.
+//
+
+import Testing
+@testable import BookInfo
+
+struct BookInfoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
